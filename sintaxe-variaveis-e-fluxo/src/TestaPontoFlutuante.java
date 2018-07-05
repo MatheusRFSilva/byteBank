17622853d809654eb7f58f76667b7aee2f74b954
Matheus Rodrigues <matheusuniversal@yahoo.com.br>
2018-07-05 16:43:33 -0300

    commit inicial

---
+++ sintaxe-variaveis-e-fluxo/src/TestaPontoFlutuante.java
@@ -0,0 +1,8 @@
+
+public class TestaPontoFlutuante {
+	public static void main(String[] args) {
+		//double novaTentativa = 5/2;
+		double novaTentativa = 5.0/2;
+		System.out.println(novaTentativa);
+	}
+}
